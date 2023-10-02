29f23ec05b0f64c4dfb7e75745ea86955e070b67
DoomKitty87 <doomkitty887@gmail.com>
2023-10-02 14:24:40 -0700

    added support for mcqs on all question types

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const problemExplanation = document.getElementById("explanation");
 var lastRequestedProblem = "";
 var currentType = 0;
 var types = document.getElementById("headerbuttons").children;
+const mcq = document.getElementById("mcq");
 
 const problemTypes = [["Buoyant Force Submerged", "Buoyant Force Floating", "Fluid Flow Conservation"], ["Power Rule"], ["P Value Conclusion"]];
 
@@ -22,7 +23,7 @@ function generateProblem() {
 
 function getProblem(problemSet) {
   currentExplanation = "";
-  const problem = window[problemSet]();
+  const problem = window[problemSet](mcq.checked);
   problemExplanation.style.display = "none";
   lastRequestedProblem = problemSet;
   answerForm.innerHTML = "";
@@ -49,7 +50,7 @@ function getProblem(problemSet) {
     input.name = "answer";
     input.id = currentPossibleAnswers[i];
     label.for = i;
-    label.innerHTML = currentPossibleAnswers[i].charAt(0).toUpperCase() + currentPossibleAnswers[i].slice(1);
+    label.innerHTML = currentPossibleAnswers[i];
     label.classList += "answerlabel";
     answerForm.appendChild(label);
     answerForm.appendChild(input);
@@ -101,6 +102,19 @@ function getRandomValue(mean, variance, decimals) {
   return Math.round((mean + (Math.random() - 0.5) * variance) * Math.pow(10, decimals)) / Math.pow(10, decimals);
 }
 
+function generateMCQAnswers(answer) {
+  answers = [];
+
+  answers[Math.floor(Math.random() * 4)] = answer;
+  for (var i = 0; i < 4; i++) {
+    if (answers[i] == null) {
+      answers[i] = (Math.round(answer * getRandomValue(1, 1, 2) * 100) / 100).toString();
+    }
+  }
+
+  return answers;
+}
+
 function setType(type) {
   currentType = type;
   problemOptions.innerHTML = "";
@@ -139,37 +153,41 @@ async function spawnConfetti() {
   confettiTemp.remove();
 }
 
-function buoyantForceSubmerged() {
+function buoyantForceSubmerged(mcq) {
   const mass = getRandomValue(200, 150, 2);
   const sideLength = getRandomValue(0.5, 0.2, 2);
   const density = getRandomValue(1000, 300, 1);
-  currentAnswer = [(Math.round(Math.abs(mass * 10 - sideLength * sideLength * sideLength * density * 10) * 100) / 100).toString()];
-  currentPossibleAnswers = ["shortresponse"];
+  const answer = Math.round(Math.abs(mass * 10 - sideLength * sideLength * sideLength * density * 10) * 100) / 100;
+  currentAnswer = [answer.toString()];
+  if (mcq) currentPossibleAnswers = generateMCQAnswers(answer);
+  else currentPossibleAnswers = ["shortresponse"];
   currentExplanation = `We want to find the net force acting on the cube, which will be the forces in one direction subtracted from the forces in the opposite, as we do not have any horizontal forces. The force down is equal to the weight of the cube, or m * g (${mass} * 10). The force up is equal to the buoyant force, or p<sub>fluid</sub> * g * V<sub>submerged</sub>. Since we know all of the cube is submerged, we can calculate the buoyant force as ${density} * 10 * ${Math.round(sideLength * sideLength * sideLength * 1000) / 1000}. Subtracting the two gives us the net force of ${currentAnswer[0]}N.`;
   return `There is a cube with a mass of ${mass}kg and side length ${sideLength}m submerged in a fluid of density ${density}kg/m<sup>3</sup>. Find the magnitude of the net force acting on the cube in N. (g = 10, round to 2 decimals)`;
 }
 
-function buoyantForceFloating() {
+function buoyantForceFloating(mcq) {
   const sideLength = getRandomValue(0.5, 0.2, 2);
   const density = getRandomValue(1000, 300, 1);
   const depth = getRandomValue(sideLength / 2, sideLength / 2, 2);
   currentAnswer = [(Math.round(sideLength * sideLength * depth * density * 100) / 100).toString()];
-  currentPossibleAnswers = ["shortresponse"];
+  if (mcq) currentPossibleAnswers = generateMCQAnswers(answer);
+  else currentPossibleAnswers = ["shortresponse"];
   currentExplanation = `We know the buoyant force here is equal to the weight of the cube, because it is floating in equilibrium. Because the buoyant force is equal to the g * p<sub>fluid</sub> * V<sub>submerged</sub>, we can set these two forces equal to each other, which gives us g * m<sub>cube</sub> = g * p<sub>fluid</sub> * V<sub>submerged</sub>. Dividing out g from both sides leaves us with the mass of the cube on the left, and we just need to find the submerged volume. Since we know the side length, the cross-sectional area of the cube is ${sideLength} * ${sideLength} = ${Math.round(sideLength * sideLength * 100) / 100}m<sup>2</sup>, and we can multiply that by the depth to get ${Math.round(sideLength * sideLength * depth * 100) / 100}m<sup>3</sup> of submerged volume. Multiplying that value by the density of the fluid gives us the mass, which is ${Math.round(sideLength * sideLength * depth * density * 100) / 100}kg.`;
   return `There is a cube with side length ${sideLength}m floating in a fluid of density ${density}kg/m3 with its bottom edge ${depth}m below the surface. Find the mass of the cube in kg. (g = 10, round to 2 decimals)`;
 }
 
-function fluidFlowConservation() {
+function fluidFlowConservation(mcq) {
   const dia1 = getRandomValue(2, 2, 1);
   const dia2 = getRandomValue(2, 2, 1);
   const vel1 = getRandomValue(10, 12, 1);
   currentAnswer = [(Math.round(vel1 * (Math.pow(dia1 / 2, 2) / Math.pow(dia2 / 2, 2)) * 100) / 100).toString()];
-  currentPossibleAnswers = ["shortresponse"];
+  if (mcq) currentPossibleAnswers = generateMCQAnswers(answer);
+  else currentPossibleAnswers = ["shortresponse"];
   currentExplanation = `According to the conservation of flow rate, the total volume flow rate (for an incompressible fluid) at one point in a pipe must equal the flow rate at another point. We can calculate the ratio between the cross-sectional area of A and B by (d<sub>A</sub> / 2)<sup>2</sup> / (d<sub>B</sub> / 2)<sup>2</sup>, which equals ${Math.round(Math.pow(dia1 / 2, 2) / Math.pow(dia2 / 2, 2) * 100) / 100}. We can then multiply the velocity of point A by this value to give us the velocity at point B, which is ${currentAnswer[0]}cm/s.`;
   return `A fluid is moving through a pipe from location A to location B. At location A, the pipe's diameter is ${dia1}cm, and the fluid flows at ${vel1}cm/s. At location B, the pipe's diameter is ${dia2}cm. Find the velocity of the fluid at location B in cm/s.`;
 }
 
-function pValueConclusion() {
+function pValueConclusion(mcq) {
   const pValue = getRandomValue(0.2, 0.3999, 3);
   const alphaValue = [0.01, 0.05, 0.1, 0.15][getRandomValue(2, 2, 0)];
   currentAnswer = [pValue > alphaValue ? "true" : "false"];
@@ -177,7 +195,7 @@ function pValueConclusion() {
   return `An experiment's z/t-score resulted in a p-value of ${pValue}, and the designers of the experiment chose to use an alpha value of ${alphaValue}. Is there enough evidence to reject the null hypothesis? (true or false)`;
 }
 
-function powerRule() {
+function powerRule(mcq) {
   const terms = getRandomValue(3, 4, 0);
   var coefficients = [];
   var exponents = [];
@@ -248,7 +266,8 @@ function powerRule() {
 
   expression = "f(x) = " + expression + ". Find f'(x).";
   currentAnswer = [answer];
-  currentPossibleAnswers = ["shortresponse"];
+  if (mcq) currentPossibleAnswers = generateMCQAnswers(answer);
+  else currentPossibleAnswers = ["shortresponse"];
   currentExplanation = `We can use the power rule to find the derivative of each term, then add them together. The power rule states that the derivative of x<sup>n</sup> is n * x<sup>n - 1</sup>. Therefore, f'(x) = ${formattedAnswer}.`;
   return expression;
 }
