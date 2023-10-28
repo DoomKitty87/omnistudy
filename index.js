4c98d29d8f3c625ef953f6ed9a8fbd900bab245e
DoomKitty87 <doomkitty887@gmail.com>
2023-10-27 22:48:05 -0700

    fixed copy button

--- index.js
+++ index.js
@@ -499,6 +499,10 @@ function generateMCQAnswers(answer, text = false) {
   return answers;
 }
 
+function copyCurrent() {
+  navigator.clipboard.writeText(problemText.innerHTML);
+}
+
 function setProblemSet(indx) {
   currentProblem = indx;
   topOption.innerHTML = document.getElementById("problemoptions").children[parseInt(indx) + 2].innerHTML + "<span style='font-weight:300;position:absolute;font-size:2.5rem;margin-left:0.5rem;margin-top:0.1rem;'>&#x2C7;</span>";
